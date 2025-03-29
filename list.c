274268de82d103c1f8d57f2d3a116c42aab8cd1c
Your Name <you@example.com>
2025-03-29 19:18:43 -0300

    new try

--- list.c
+++ list.c
@@ -43,7 +43,7 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-    if (list->current == NULL || list->current->next == NULL) return NULL;
+    if (list->current->next == NULL) return NULL;
     list->current = list->current->next;
     return list->current->data;
 }
@@ -57,8 +57,9 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
-    
-    return NULL;
+    if (list->current->prev == NULL) return NULL;
+    list->current = list->current->prev;
+    return list->current->data;
 }
 
 void pushFront(List * list, void * data) {
